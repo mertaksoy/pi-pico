95d7799c434e09adb087f32369a9a48af11de359
Mert Aksoy <mert.aksoy@codesquare.at>
2021-11-10 00:08:45 +0300

    Displaying text

--- main.py
+++ main.py
@@ -1,90 +1,166 @@
-from machine import Pin
-from time import sleep
-import _thread
+from micropython import const
+import utime as time
+import framebuf
+from machine import Pin, I2C
 
-dp = Pin(0, Pin.OUT)
-cl = Pin(1, Pin.OUT)
-la = Pin(2, Pin.OUT)
-clr = Pin(3, Pin.OUT, Pin.PULL_UP)
-
-dots = Pin(4, Pin.OUT)
-
-dp.value(0)
-cl.value(0)
-la.value(0)
-
-num_0 = 0b00000011
-num_1 = 0b10011111
-num_2 = 0b00100101
-num_3 = 0b00001101
-num_4 = 0b10011001
-num_5 = 0b01001001
-num_6 = 0b01000001
-num_7 = 0b00011111
-num_8 = 0b00000001
-num_9 = 0b00001001
-
-numbers = [num_0, num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8, num_9]
+# a few register definitions
+_SET_CONTRAST = const(0x81)
+_SET_NORM_INV = const(0xa6)
+_SET_DISP = const(0xae)
+_SET_SCAN_DIR = const(0xc0)
+_SET_SEG_REMAP = const(0xa0)
+_LOW_COLUMN_ADDRESS = const(0x00)
+_HIGH_COLUMN_ADDRESS = const(0x10)
+_SET_PAGE_ADDRESS = const(0xB0)
 
 
-def clear():
-    clr.low()
-    clr.high()
+class SH1106:
+    def __init__(self, width, height, external_vcc):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        fb = framebuf.FrameBuffer(self.buffer, self.width, self.height,
+                                  framebuf.MVLSB)
+        self.framebuf = fb
+        # set shortcuts for the methods of framebuf
+        self.fill = fb.fill
+        self.fill_rect = fb.fill_rect
+        self.hline = fb.hline
+        self.vline = fb.vline
+        self.line = fb.line
+        self.rect = fb.rect
+        self.pixel = fb.pixel
+        self.scroll = fb.scroll
+        self.text = fb.text
+        self.blit = fb.blit
 
+        self.init_display()
 
-clear()
+    def init_display(self):
+        self.reset()
+        self.fill(0)
+        self.poweron()
+        self.show()
 
+    def poweroff(self):
+        self.write_cmd(_SET_DISP | 0x00)
 
-def tick():
-    cl.low()
-    cl.high()
+    def poweron(self):
+        self.write_cmd(_SET_DISP | 0x01)
 
-
-def latch():
-    la.high()
-    la.low()
-
-
-def write(value):
-    for i in range(16):
-        data = value >> i & 1
-        if data == 0:
-            dp.high()
+    def rotate(self, flag, update=True):
+        if flag:
+            self.write_cmd(_SET_SEG_REMAP | 0x01)  # mirror display vertically
+            self.write_cmd(_SET_SCAN_DIR | 0x08)  # mirror display hor.
         else:
-            dp.low()
-        tick()
-    latch()
-    sleep(0.5)
+            self.write_cmd(_SET_SEG_REMAP | 0x00)
+            self.write_cmd(_SET_SCAN_DIR | 0x00)
+        if update:
+            self.show()
+
+    def sleep(self, value):
+        self.write_cmd(_SET_DISP | (not value))
+
+    def contrast(self, contrast):
+        self.write_cmd(_SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(_SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        for page in range(self.height // 8):
+            self.write_cmd(_SET_PAGE_ADDRESS | page)
+            self.write_cmd(_LOW_COLUMN_ADDRESS | 2)
+            self.write_cmd(_HIGH_COLUMN_ADDRESS | 0)
+            self.write_data(self.buffer[
+                            self.width * page:self.width * page + self.width
+                            ])
+
+    def reset(self, res):
+        if res is not None:
+            res(1)
+            time.sleep_ms(1)
+            res(0)
+            time.sleep_ms(20)
+            res(1)
+            time.sleep_ms(20)
 
 
-def toBit(number):
-    if number < 10:
-        return numbers[0] << 8 | numbers[number]
-    elif number > 9 & number < 100:
-        digits = toDigits(number)
-        return numbers[digits[0]] << 8 | numbers[digits[1]]
-    else:
-        return numbers[0] << 8 | numbers[0]
+class SH1106_I2C(SH1106):
+    def __init__(self, width, height, i2c, res=None, addr=0x3c,
+                 external_vcc=False):
+        self.i2c = i2c
+        self.addr = addr
+        self.res = res
+        self.temp = bytearray(2)
+        if res is not None:
+            res.init(res.OUT, value=1)
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80  # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_data(self, buf):
+        self.i2c.writeto(self.addr, b'\x40' + buf)
+
+    def reset(self):
+        super().reset(self.res)
 
 
-def toDigits(number):
-    return list(map(int, ' '.join(str(number)).split()))
+class SH1106_SPI(SH1106):
+    def __init__(self, width, height, spi, dc, res=None, cs=None,
+                 external_vcc=False):
+        self.rate = 10 * 1000 * 1000
+        dc.init(dc.OUT, value=0)
+        if res is not None:
+            res.init(res.OUT, value=0)
+        if cs is not None:
+            cs.init(cs.OUT, value=1)
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs = cs
+        super().__init__(width, height, external_vcc)
 
-
-def blink_clock_dots():
-    dots_high = False
-    while True:
-        if dots_high:
-            dots.low()
+    def write_cmd(self, cmd):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            self.cs(1)
+            self.dc(0)
+            self.cs(0)
+            self.spi.write(bytearray([cmd]))
+            self.cs(1)
         else:
-            dots.high()
-        dots_high = not dots_high
-        sleep(1)
+            self.dc(0)
+            self.spi.write(bytearray([cmd]))
+
+    def write_data(self, buf):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            self.cs(1)
+            self.dc(1)
+            self.cs(0)
+            self.spi.write(buf)
+            self.cs(1)
+        else:
+            self.dc(1)
+            self.spi.write(buf)
+
+    def reset(self):
+        super().reset(self.res)
 
 
-_thread.start_new_thread(blink_clock_dots, ())
+i2c = I2C(0, sda=Pin(4), scl=Pin(5), freq=400000)
+display = SH1106_I2C(128, 64, i2c, Pin(2), 0x3c)
+display.sleep(False)
 
 while True:
-    for i in range(99):
-        write(toBit(i))
-        sleep(1)
+    for i in range(64):
+        display.fill(0)
+        display.text('Mert Aksoy', 0, i, 1)
+        display.show()
